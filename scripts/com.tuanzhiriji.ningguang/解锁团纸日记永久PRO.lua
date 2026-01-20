fa9223a5742d42b7e254ef601b24ef77c462d98b
怀天 <2965373537@qq.com>
2026-01-20 11:02:12 +0800

    解锁团纸日记永久PRO
    解锁团纸日记永久PRO

---
+++ scripts/com.tuanzhiriji.ningguang/解锁团纸日记永久PRO.lua
@@ -0,0 +1,28 @@
+-- name: 解锁团纸日记永久PRO
+-- descript: 解锁团纸日记永久PRO
+-- package: com.tuanzhiriji.ningguang
+-- author: 怀天
+
+hook {
+  class = "com.tuanzhiriji.ningguang.app.activity.MyApplication",
+  classloader = lpparam.classLoader,
+  method = "getPermanentPro",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = true
+  end,
+}hook {
+  class = "com.tuanzhiriji.ningguang.app.base.BaseActivity",
+  classloader = lpparam.classLoader,
+  method = "isPro",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = true
+  end,
+}
