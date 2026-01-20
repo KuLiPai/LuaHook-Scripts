2244d77e79dbd4a0f8a8cc3d9b2b1ad0d4793de6
怀天 <2965373537@qq.com>
2026-01-20 10:30:37 +0800

    Add script to unlock flowmark permanent membership
    This script unlocks permanent membership by modifying the results of specific methods in the UserInfo class.

---
+++ scripts/com.fewwind.flowmark/永久会员.lua
@@ -0,0 +1,39 @@
+-- name: 解锁永久会员
+-- descript: 解锁本地永久会员
+-- package: com.fewwind.flowmark
+-- author: 怀天
+
+hook {
+  class = "com.fewwind.flowmark.data.user.UserInfo",
+  classloader = lpparam.classLoader,
+  method = "getOrderId",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = "isVip"
+  end,
+}hook {
+  class = "com.fewwind.flowmark.util.r$a",
+  classloader = lpparam.classLoader,
+  method = "a",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = "pro2"
+  end,
+}hook {
+  class = "com.fewwind.flowmark.data.user.UserInfo",
+  classloader = lpparam.classLoader,
+  method = "getProType",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = "pro2"
+  end,
+}
