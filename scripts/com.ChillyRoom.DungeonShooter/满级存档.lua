08dae49162c05ef3fe84233c53d03dfcbc1b185e
怀天 <2965373537@qq.com>
2026-01-20 21:29:57 +0800

    元气骑士满级云存档
    元气骑士满级云存档

---
+++ scripts/com.ChillyRoom.DungeonShooter/满级存档.lua
@@ -0,0 +1,21 @@
+-- name: 满级存档
+-- descript: 满级存档
+-- package: com.ChillyRoom.DungeonShooter
+-- author: 怀天
+
+hook {
+  class = "com.chillyroomsdk.sdkbridge.BasePlayerActivity",
+  classloader = lpparam.classLoader,
+  method = "onCreate",
+  params = {findClass("android.os.Bundle", lpparam.classLoader)},
+  before = function(it)
+    imports "android.widget.Toast"
+    local activity = it.thisObject
+    Toast.makeText(activity,"账号: guurgc@163.com",Toast.LENGTH_LONG).show()
+    Toast.makeText(activity,"密码: xtkvqs",Toast.LENGTH_LONG).show()
+    Toast.makeText(activity,"云端下载到本地即可体验",Toast.LENGTH_LONG).show()
+  end,
+  after = function(it)
+      
+  end,
+}
