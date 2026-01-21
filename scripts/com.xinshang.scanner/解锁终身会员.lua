d014ab6ba9557c7703829946837470d47b9fb269
怀天 <2965373537@qq.com>
2026-01-21 12:21:52 +0800

    扫描专家(ver. 1.6.1) 终身会员

---
+++ scripts/com.xinshang.scanner/解锁终身会员.lua
@@ -0,0 +1,39 @@
+-- name: 解锁终身会员
+-- descript: 解锁终身会员(ver. 1.6.1)
+-- package: com.xinshang.scanner
+-- author: 怀天
+
+hook {
+  class = "com.xinshang.scanner.module.ucenter.objects.ScannerUserVIPInfo",
+  classloader = lpparam.classLoader,
+  method = "h",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = true
+  end,
+}hook {
+  class = "com.xinshang.scanner.module.ucenter.objects.ScannerUserVIPInfo",
+  classloader = lpparam.classLoader,
+  method = "x",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = false
+  end,
+}hook {
+  class = "com.xinshang.scanner.module.ucenter.objects.ScannerUserVIPInfo",
+  classloader = lpparam.classLoader,
+  method = "q",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = true
+  end,
+}
