bedf156b4f90da1154e7f5e042577469fc233001
怀天 <2965373537@qq.com>
2026-01-21 22:28:55 +0800

    开练 永久会员

---
+++ scripts/com.yuanlue.sportnote/解锁永久会员.lua
@@ -0,0 +1,40 @@
+-- name: 解锁永久会员
+-- descript: 解锁永久会员(ver. 7.0.4)
+-- package: com.yuanlue.sportnote
+-- author: 怀天
+
+hook {
+ class="android.app.Application",
+ method="attach",
+ params={"android.content.Context",},
+ after=function(it)
+   local loader = invoke(it.thisObject, "getClassLoader")
+   
+   
+   hook {
+     class="com.yuanlue.sportnote.login.UserInfo$UserData",
+     method="isVip",
+     classloader=loader,
+     params={},
+     before=function(it)
+
+     end,
+     after=function(it)
+         it.result = true
+     end
+   }hook {
+     class="com.yuanlue.sportnote.login.UserInfo$UserData",
+     method="isForeverVip",
+     classloader=loader,
+     params={},
+     before=function(it)
+
+     end,
+     after=function(it)
+         it.result = true
+     end
+   }
+
+
+ end
+}
