18c1c70845e2027e04645580ef4708f0f14ff722
怀天 <2965373537@qq.com>
2026-01-21 11:09:15 +0800

    指尖时光永久会员
    指尖时光永久会员

---
+++ scripts/com.zjzy.calendartime/永久会员.lua
@@ -0,0 +1,17 @@
+-- name: 永久会员
+-- descript: 解锁永久会员
+-- package: com.zjzy.calendartime
+-- author: 怀天
+
+hook {
+  class = "com.app.modelintegral.data.bean.UserInfoBean",
+  classloader = lpparam.classLoader,
+  method = "getVIP",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = int(2)
+  end,
+}
