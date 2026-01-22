8b8daf862ea507a8368d96fbdc52a747e5509ca9
怀天 <2965373537@qq.com>
2026-01-22 13:44:28 +0800

    朝花夕拾 永久高级版

---
+++ scripts/com.magicalstory.days/解锁永久高级版.lua
@@ -0,0 +1,28 @@
+-- name: 解锁永久高级版
+-- descript: 解锁永久高级版(ver. 1.3.4)
+-- package: com.magicalstory.days
+-- author: 怀天
+
+hook {
+  class = "bb.h",
+  classloader = lpparam.classLoader,
+  method = "d",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = "永久高级版"
+  end,
+}hook {
+  class = "bb.h",
+  classloader = lpparam.classLoader,
+  method = "j",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = true
+  end,
+}
