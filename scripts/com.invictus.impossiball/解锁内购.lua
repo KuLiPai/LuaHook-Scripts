c0a94a11a5312a1a4f39be52c8aef1748aa650ce
怀天 <2965373537@qq.com>
2026-01-22 18:09:28 +0800

    永不言弃 解锁内购

---
+++ scripts/com.invictus.impossiball/解锁内购.lua
@@ -0,0 +1,17 @@
+-- name: 解锁内购
+-- descript: 解锁支付宝内购(ver. 5.1.3)
+-- package: com.invictus.impossiball
+-- author: 怀天
+
+hook {
+  class = "com.east2west.sdk.AliPay.PayResult",
+  classloader = lpparam.classLoader,
+  method = "getResultStatus",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = "9000"
+  end,
+}
