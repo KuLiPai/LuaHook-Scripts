f8b104c368ad209188f10acc7629414413754848
怀天 <2965373537@qq.com>
2026-01-23 22:40:05 +0800

    句子控 本地SVIP

---
+++ scripts/com.wordscon.axe/解锁本地SVIP.lua
@@ -0,0 +1,51 @@
+-- name: 解锁本地SVIP
+-- descript: 解锁本地SVIP(ver. 6.0.1)
+-- package: com.wordscon.axe
+-- author: 怀天
+
+hook {
+ class="android.app.Application",
+ method="attach",
+ params={"android.content.Context",},
+ after=function(it)
+   local loader = invoke(it.thisObject, "getClassLoader")
+   
+   
+   hook {
+     class="com.wordscon.axe.auth.AXUserManager$Companion",
+     method="isMember",
+     classloader=loader,
+     params={},
+     before=function(it)
+
+     end,
+     after=function(it)
+         it.result = true
+     end
+   }hook {
+     class="com.wordscon.axe.auth.AXUserManager$Companion",
+     method="isSuperMember",
+     classloader=loader,
+     params={},
+     before=function(it)
+
+     end,
+     after=function(it)
+         it.result = true
+     end
+   }hook {
+     class="com.wordscon.axe.auth.AXUserManager$Companion",
+     method="isPermanentMember",
+     classloader=loader,
+     params={},
+     before=function(it)
+
+     end,
+     after=function(it)
+         it.result = true
+     end
+   }
+
+
+ end
+}
