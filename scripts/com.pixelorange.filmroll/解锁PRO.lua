5bf8249aafc96f1fd87b35c9390fafb5362d7d03
怀天 <2965373537@qq.com>
2026-01-23 22:41:39 +0800

    最后一卷胶片 解锁PRO

---
+++ scripts/com.pixelorange.filmroll/解锁PRO.lua
@@ -0,0 +1,17 @@
+-- name: 解锁PRO
+-- descript: 解锁PRO(ver. 2.17)
+-- package: com.pixelorange.filmroll
+-- author: 怀天
+
+hook {
+  class = "com.pixelorange.filmroll.utils.UserDefaultUtils",
+  classloader = lpparam.classLoader,
+  method = "getProState",
+  params = {},
+  before = function(it)
+
+  end,
+  after = function(it)
+    it.result = true
+  end,
+}
